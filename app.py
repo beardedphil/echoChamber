a39dbcee388ddb2033cba31b284edeb5412a5f75
Philip King <ilovethechiefs@gmail.com>
2017-08-29 15:26:10 -0500

    Heroku deploy failing...

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,13 +10,3 @@ def index():
 
 if __name__ == '__main__':
 	app.run()
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-# if __name__ == '__main__':
-#     app.debug = True
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
