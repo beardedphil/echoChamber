5dbbaad619ec652354206a8a6661ff60cb7e2910
Philip King <ilovethechiefs@gmail.com>
2017-08-30 13:32:12 -0500

    Added links for users, articles, register, and login.  Routes and templates added as well

--- app.py
+++ app.py
@@ -12,9 +12,28 @@ db.init_app(app)
 
 @app.route("/")
 def index():
+	return render_template("index.html")
+
+@app.route("/users")
+def users():
 	user_list = User.query.all()
 	print(user_list)
-	return render_template("index.html", user_list=user_list)
+	return render_template("users.html", user_list=user_list)
+
+@app.route("/articles")
+def articles():
+	# articles = Article.query.all()
+	# print(articles)
+	# return render_template("articles.html", articles=articles)
+	return render_template("articles.html")
+
+@app.route("/register")
+def register():
+	return render_template("register.html")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
 
 if __name__ == '__main__':
 	app.run()
