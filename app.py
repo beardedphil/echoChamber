f39b55db202f0309831b9fd89e7b3b2f64d32f33
Philip King <ilovethechiefs@gmail.com>
2017-08-31 17:59:12 -0500

    Updated models.  Articles can now be loaded in the background.  Articles and authors are stored in separate tables.  Minimal styling on /articles.html.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 
 from flask import Flask, render_template, url_for
 from models import *
+import newspaper
+import threading
+import os
 
 app = Flask(__name__)
 
@@ -22,10 +25,10 @@ def users():
 
 @app.route("/articles")
 def articles():
-	# articles = Article.query.all()
-	# print(articles)
-	# return render_template("articles.html", articles=articles)
-	return render_template("articles.html")
+	thread = threading.Thread(target=threaded_get_articles)
+	thread.start()
+
+	return render_template("articles.html", articles=Article.query.all())
 
 @app.route("/register")
 def register():
@@ -34,6 +37,51 @@ def register():
 @app.route("/login")
 def login():
 	return render_template("login.html")
+
+def threaded_get_articles():
+	with app.app_context():
+		site = "cnn.com"
+		cnn_paper = newspaper.build('http://' + site, memoize_articles=False)
+		for article in cnn_paper.articles:
+			try:
+				article.download()
+				article.parse()
+				article.nlp()
+			except:
+				continue
+
+			if not article.title or article.title == "None":
+				print("No title")
+			elif not article.url:
+				print("No url")
+			elif not article.text or article.text == "None" or article.text == "":
+				print("No text")
+			else:
+				new_article = Article(article.title, article.url, article.text)
+
+				if article.top_image:
+					new_article.image_link = article.top_image
+				elif article.images:
+					new_article.image_link = article.images[0]
+				else:
+					new_article.image_link = NULL
+
+				if article.summary:
+					new_article.summary = article.summary
+				else:
+					new_article.summary = "Summary not available"
+
+				logo_link = "//logo.clearbit.com/" + site
+				new_article.logo_link = logo_link
+
+				db.session.add(new_article)
+				db.session.commit()
+
+				for author in article.authors:
+					new_author = Author(new_article.id, author)
+					db.session.add(new_author)
+
+				db.session.commit()
 
 if __name__ == '__main__':
 	app.run()
--- models.py
+++ models.py
@@ -12,3 +12,38 @@ class User(db.Model):
     def __init__(self, username, hash):
         self.username = username
         self.hash = hash
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    url = db.Column(db.Text, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    image_link = db.Column(db.Text, nullable=True)
+    logo_link = db.Column(db.Text, nullable=True)
+    summary = db.Column(db.Text, nullable=True)
+
+    def __init__(self, title, url, text):
+        self.title = title
+        self.url = url
+        self.text = text
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    article_id = db.Column(db.Integer, nullable=False)
+    author = db.Column(db.Text, nullable=False)
+
+    def __init__(self, article_id, author):
+        self.article_id = article_id
+        self.author = author
+
+class Image(db.Model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    article_id = db.Column(db.Integer, nullable=False)
+    image_link = db.Column(db.Text, nullable=False)
+
+    def __init__(self, article_id, image_link):
+        self.article_id = article_id
+        self.image = image
