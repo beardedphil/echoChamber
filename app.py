0e59b8f31cc1649c95a849c8fafdf872f7097415
Philip King <ilovethechiefs@gmail.com>
2017-09-01 14:11:42 -0500

    Registration and login

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
 from models import *
+
 import newspaper
 import threading
 import os
+import urllib.request
 
 app = Flask(__name__)
 
@@ -12,6 +17,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///echo.db'
 app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -30,12 +41,62 @@ def articles():
 
 	return render_template("articles.html", articles=Article.query.all())
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    # if user reached route via POST (as by submitting a form via POST)
+	if request.method == "POST":
+        # ensure username was submitted
+		if not request.form.get("username"):
+			return render_template("register.html", error_message="must choose a username")
+		elif not request.form.get("password"):
+			return render_template("register.html", error_message="must choose a password")
+		elif not (request.form.get("password") == request.form.get("password2")):
+			return render_template("register.html", error_message="passwords must match")
+
+        # query database for username
+		username = request.form.get("username")
+		rows = User.query.filter_by(username = username).first()
+
+        # ensure username does not already exist
+		if rows != None:
+			return render_template("register.html", error_message="that username is already taken")
+		else:
+			user = User(username, pwd_context.hash(request.form.get("password")))
+			db.session.add(user)
+			db.session.commit()
+			return render_template("login.html")
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
 	return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+	session.clear()
+
+	if request.method == "POST":
+
+		# ensure username was submitted
+		if not request.form.get("username"):
+			return render_template("login.html", error_message="must provide username")
+
+		# ensure password was submitted
+		if not request.form.get("password"):
+			return render_template("login.html", error_message="must provide password")
+
+		# query database for username
+		user = User.query.filter_by(username=request.form.get("username")).first()
+
+		# ensure username exists and password is correct
+		if user == None or not pwd_context.verify(request.form.get("password"), user.hash):
+			return render_template("login.html", error_message="invalid username and/or password")
+
+		# remember which user has logged in
+		session["user_id"] = user.id
+
+		# redirect user to home page
+		return redirect(url_for("index"))
+
+	# else if user reached route via GET (as by clicking a link or via redirect)
 	return render_template("login.html")
 
 def threaded_get_articles():
