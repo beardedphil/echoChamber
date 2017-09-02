41f1015e17d3cd240be81622aa2ac8159f915488
Philip King <ilovethechiefs@gmail.com>
2017-09-01 20:44:39 -0500

    Removed callback from login_required function.  Not being used at this time.

--- helpers.py
+++ helpers.py
@@ -20,6 +20,6 @@ def login_required(f):
 	@wraps(f)
 	def decorated_function(*args, **kwargs):
 		if session.get("user_id") is None:
-			return redirect(url_for("login", next=request.url))
+			return redirect(url_for("login"))
 		return f(*args, **kwargs)
 	return decorated_function
